ab07cc5a4f6653dfaf82452e07a655f307f40372
aksite7 <foxieting@gmail.com>
2024-07-27 01:12:11 +0300

    Попытка вывести иконку x2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <meta property="og:description" content="Специализированный онлайн магазин, который предлагает ассортимент дроби и картечи высокого качества для охотников, спортсменов и любителей стрелкового дела. На нашем сайте вы найдете разнообразные виды дроби и картечи. Мы гарантируем только оригинальную продукцию, которая отличается надежностью, точностью и безопасностью использования. Доверьтесь опыту и надежности VSHOT и наслаждайтесь покупками без лишних хлопот!"/>
         <meta property="og:url" content="https://volgashot.ru/"/>
         <link rel='icon' href='/favicon.ico' type="image/x-icon"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" type="image/x-icon"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" type="image/x-icon"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" type="image/x-icon"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <link rel="manifest" href="/site.webmanifest"/>
       </head>
       <body className={inter.className + ' bg-background text-gray-400 text-base overflow-x-hidden'}>
