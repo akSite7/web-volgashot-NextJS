6451d06c0d406afe2650693a3b00e3f0fd3ba3a2
aksite7 <foxieting@gmail.com>
2024-07-27 19:59:04 +0300

    Добавил соотношения сторон

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,18 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     backgroundImage: {
       gun: 'url(/assets/img/intro/gun.svg)',
     },
@@ -17,5 +23,4 @@ const config: Config = {
     },
   },
   plugins: [],
-};
-export default config;
+}
