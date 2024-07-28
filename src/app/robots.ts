5d66fedbec44a17a293872972f1cd2611152bb2e
aksite7 <foxieting@gmail.com>
2024-07-28 19:55:58 +0300

    Добавил генерацию robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  
+  return {
+    rules: [
+      {
+        userAgent: "yandex",
+        allow: "/",
+        disallow: ["/fractions", "/buckshots", "/other"],
+      }
+    ],
+    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+  }
+  
+}
