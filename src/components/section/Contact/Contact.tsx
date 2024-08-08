c963894cec737963066f4ccc502621d5369bc3de
akSite <132459621+akSite7@users.noreply.github.com>
2024-08-08 21:16:05 +0300

    Финальный продукт

--- src/components/section/Contact/Contact.tsx
+++ src/components/section/Contact/Contact.tsx
@@ -15,7 +15,7 @@ const Contact = () => {
           <h3 itemProp="name" className={styles.contactTitleUr}>ООО ПК «ВОЛГА ШОТ-М»</h3>
           <ul className={styles.contactItems}>
             <li className={styles.contactItem}>
-              <span className={styles.contactSpan}>Юридический адрес: </span>
+              <span className={styles.contactSpan}>Расположение: </span>
               <span itemProp="streetAddress">400079, Волгоградская Область, г.о. Город-герой Волгоград, г Волгоград, ул Самарская, влд. 1, офис 1</span>
             </li>
             {ContactList.map(( item, index ) => {
@@ -26,7 +26,7 @@ const Contact = () => {
               )
             })}
             <li className={styles.contactItem}>
-              <span className={styles.contactSpan}>Главный бухгалтер: </span>
+              <span className={styles.contactSpan}>Способы связи: </span>
               <span itemProp="telephone">+7 978 588-06-48, </span>
               <span itemProp="faxNumber">+7 961 685-32-12</span>
             </li>
