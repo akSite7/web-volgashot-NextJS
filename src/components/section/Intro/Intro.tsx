6884a51adfd19d00527396c6b770352c7872f143
akSite <132459621+akSite7@users.noreply.github.com>
2024-11-27 04:02:21 +0300

    Изменил ссылку на отзывы

--- src/components/section/Intro/Intro.tsx
+++ src/components/section/Intro/Intro.tsx
@@ -22,7 +22,7 @@ const Intro = () => {
                       <Link href={'#assortment'} className={styles.introActionPrimary}>Посмотреть ассортимент</Link>
                     </li>
                     <li className={styles.introActionItem}> 
-                      <Link target="blank" href={'https://www.ozon.ru/product/drob-ohotnichya-svintsovaya-5-2kg-656480919/?_bctx=CAMQ6vKSMA&asb=nuYIZjlG4Gsnb%252BYe75dLrGWaysTnhSNn8Qq6AwcAYDg%253D&asb2=zcwZvtEYuYCxOe71-OEdj6W8xpR1COyqjxTzPggiCHuhP9NBoMVhZVm_9M4Qlw5n&avtc=1&avte=2&avts=1718916690&tab=reviews&reviewVuuid=0185a207-9533-e834-f6a9-13a8bd27bfb9&itemId=656518150&reviewsVariantMode=2'} className={styles.introActionOutline}>Отзывы</Link>
+                      <Link target="blank" href={'https://www.ozon.ru/product/drob-ohotnichya-svintsovaya-3-shot-12kg-volgashot-1711160146/?asb2=MofHjsS1efMBm5YoIJQXxcp0n7PCx7Lj0v3tj7S47aGSM5Mo81qa5m0IS21YKgnDmeJsxj-flRrohiATbJXGzg&from_sku=1711158373&from_url=https%3A%2F%2Fwww.ozon.ru%2Fcategory%2Faksessuary-i-hranenie-oruzhiya-33147%2F%3Fcategory_was_predicted%3Dtrue%26amp%3Bdeny_category_prediction%3Dtrue%26amp%3Bfrom_global%3Dtrue%26amp%3Btext%3Dдробь+охотничья&oos_search=false&tab=reviews'} className={styles.introActionOutline}>Отзывы</Link>
                     </li>
                   </ul>
                 </div>
